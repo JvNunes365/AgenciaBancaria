cfb2cc457ec876787e42851b79839e551c6febf7
JvNunes365 <163609687+JvNunes365@users.noreply.github.com>
2024-05-16 20:00:52 -0300

    Add files via upload

---
+++ AgenciaBancaria/src/model/ContaCorrente.java
@@ -0,0 +1,9 @@
+
+package model;
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(Pessoa pessoa) {
+        super(pessoa);
+    }
+}
---
+++ AgenciaBancaria/src/model/ContaPoupanca.java
@@ -0,0 +1,10 @@
+
+package model;
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(Pessoa pessoa) {
+        super(pessoa);
+}
+
+}
